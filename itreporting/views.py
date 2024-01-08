f812c5246972dfc000360ece9f99de908e3e63c6
Ubuntu <student@lab000001.jpqljhbwd5lefptxmsbasv44pd.fx.internal.cloudapp.net>
2024-01-08 01:17:03 +0000

    wesbite working

--- itreporting/views.py
+++ itreporting/views.py
@@ -3,18 +3,22 @@ from django.shortcuts import render
 
 def home(request):
     
-    return render(request, 'itreporting/home.html')
+    return render(request, 'itreporting/home.html', {'title':
+'Welcome'})
 
 def about(request):
     
-    return render(request, 'itreporting/about.html')
+    return render(request, 'itreporting/about.html', {'title':
+'About'})
 
 def contact(request):
     
-    return render(request, 'itreporting/contact.html')
+    return render(request, 'itreporting/contact.html', {'title':
+'Contact'})
 
 def moduleslist(request):
     
-    return render(request, 'itreporting/modulelist.html')
+    return render(request, 'itreporting/modulelist.html', {'title':
+'List of Modules'})
 
 # Create your views here.
